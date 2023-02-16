b6c2e73972ba6561b89ef53283d571f3267e377e
João Marcelo <joaomarcelo588@gmail.com>
2023-02-16 08:32:00 -0300

    Change canvas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
-import { MediaMatcher } from '@angular/cdk/layout';
-import { NestedTreeControl } from '@angular/cdk/tree';
-import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
-import { MatTreeNestedDataSource } from '@angular/material/tree';
+import { Component, OnInit } from '@angular/core';
 import { TokenStorageService } from './core/auth/token-storage.service';
 
 @Component({
@@ -15,99 +12,57 @@ export class AppComponent implements OnInit {
   name = "João Marcelo";
 
   isLogged: boolean = false
-  mobileQuery: MediaQueryList;
-  private _mobileQueryListener: () => void;
 
-  fillerContent = Array.from(
-    {length: 50},
-    () =>
-      `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-       labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-       laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-       voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-       cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
-  );
-
-  fillerNav = Array.from({length: 50}, (_, i) => `Nav Item ${i + 1}`);
-
-  constructor(private tokenStorageService: TokenStorageService, changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) { 
-    this.mobileQuery = media.matchMedia('(max-width: 600px)');
-    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
-    this.mobileQuery.addListener(this._mobileQueryListener);
-  }
+  constructor(private tokenStorageService: TokenStorageService) { }
 
   menuItems = [
     {
-      label: 'Início',
+      label: 'Dashboard',
       icon: 'fi-rr-home',
-      // // visible: this.roles.some((r: any) => APP_ROLES_GROUP.GRUPO_SIMULACAO.includes(r)),
-      visible: true,
-      routerLink: '/componentes-catalog'
+      routerLink: ''
     },
     {
-      label: 'Cadastro',
-      icon: 'fi-rr-add',
-      visible: true,
+      label: 'Incomes',
+      icon: 'fi-rr-chat-arrow-grow',
       items: [
         {
-          label: 'Curso',
-          icon: 'fi-rr-graduation-cap',
-          visible: true,
-          routerLink: '/user-list'
+          label: 'Income',
+          icon: 'fi fi-rr-arrow-right',
+          routerLink: 'income'
+        }, 
+        {
+          label: 'Type Income',
+          icon: 'fi fi-rr-arrow-right',
+          routerLink: 'type-income'
         }
       ]
     },
     {
-      label: 'Inscrição',
-      icon: 'fi-rr-user',
-      visible: true,
+      label: 'Expenses',
+      icon: 'fi-rr-chat-arrow-down',
       items: [
         {
-          label: '• Lista de usuários',
-          icon: '',
-          visible: true,
-          routerLink: '/user-list'
+          label: 'Expense',
+          icon: 'fi fi-rr-arrow-right',
+          routerLink: 'expense'
+        }, 
+        {
+          label: 'Type Income',
+          icon: 'fi fi-rr-arrow-right',
+          routerLink: 'type-expense'
         }
       ]
     },
-    {
-      label: 'Contrato',
-      icon: 'fi fi-rr-document-signed',
-      visible: true,
-      items: [
-        {
-          label: 'Consultar',
-          icon: 'fi fi-rr-search',
-          visible: true,
-          routerLink: '/contrato'
-        }
-      ]
-    },
-    {
-      label: 'Financeiro',
-      icon: 'fi fi-rr-money-check',
-      visible: true,
-      items: [
-        {
-          label: 'Movimento Financeiro',
-          icon: 'fi fi-rr-search',
-          visible: true,
-          routerLink: '/movimento-financeiro'
-        }
-      ]
-    },
+
 
   ];
 
 
   ngOnInit(): void {
 
-    this.mobileQuery.removeListener(this._mobileQueryListener);
-
     if (this.tokenStorageService.getToken()) {
       this.isLogged = true;
     }
-
 
   }
 
