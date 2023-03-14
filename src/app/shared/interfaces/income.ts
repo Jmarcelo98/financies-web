55361317290e8484f9fb42fd2342a7b50613b424
João Marcelo <joaomarcelo588@gmail.com>
2023-03-14 17:17:28 -0300

    Change value in view

--- src/app/shared/interfaces/income.ts
+++ src/app/shared/interfaces/income.ts
@@ -3,6 +3,7 @@ import { ITypeIncome } from "./typeIncome";
 export interface IIncome {
     id?: number;
     dateInclusion?: Date;
+    dateReference?: Date;
     isReceived?: boolean;
     value?: number;
     typeIncome?: ITypeIncome;
